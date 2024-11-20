0351706f3426101c3089120fb95305d41ca0027a
ryan-gregoire <ryangregoire286@gmail.com>
2024-11-20 14:57:12 -0500

    Added Dasy One And 2

---
+++ Main.js
@@ -0,0 +1,88 @@
+/*
+  -> Day 1 Code
+
+const name = "Ryan James Gregoire";
+
+console.log(`Hello ${name}`);
+
+
+
+console.log("1");
+console.log("2");
+console.log("3");
+
+
+console.log("Hello My Name is Ryan");
+
+
+const sum = 2 + 3;
+console.log("2 + 3 = " + sum );
+*/
+
+/*
+	
+ -> Day 2 Code
+
+*/
+
+
+let firstName = "Ryan James";
+let lastName = "Gregoire";
+let numYears = Math.abs(2020 - 2024);
+
+
+
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(`I Have Been coding for: ${numYears} Years `);
+
+
+let dateBith = new Date("11/18/1999");
+
+const titleOfJob = "JavaScript Developer";
+
+
+
+
+/* 
+--------- Data Types -----------
+// Primitives
+->-----------
+// 1: Strings
+// 2: Numbers
+// 3: BigInt
+// 4: Symbol
+// 5: Boolean
+// 6: Undefined
+// 7: Null
+
+// Reference Types
+1: Class
+2: Dates
+3: Arrays
+4: Map And Set
+5: Objects
+6: Functions
+7: Errors
+8: Pomises
+*/ 
+
+
+
+let Name = "Ryan James"
+
+let upperName = Name.toUpperCase();
+
+console.log("Name Uppercase: " + upperName);
+
+console.log("1");
+console.log("2");
+console.log("3");
+
+let name1 = "Ryan";
+
+
+
+
+
---
+++ string.js
@@ -0,0 +1,34 @@
+let firstName = "Ryan";
+
+let lastName = 'Gregoire';
+
+let title = `Coding in JavaScript`;
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+let bioN = "Hello\n\nRyan\nGregoire";
+console.log(bioN);
+
+let excapeChars = "I love JavaScript\'s"
+
+console.log(excapeChars);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
