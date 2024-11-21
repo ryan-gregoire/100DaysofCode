0605dfcdb3ed4b6a25f8afd6e0c78d3130cfa20a
ryan-gregoire <ryangregoire286@gmail.com>
2024-11-21 13:17:43 -0500

    Added Manh Videos Code

---
+++ Day-2/Objects.js
@@ -0,0 +1,19 @@
+let ob1 = {};
+
+let oj1 = new Object();
+
+ob1.firstName = "Ryan";
+ob1.lastName = "James";
+ob1.age = 25;
+ob1.experience = Math.abs(2024 - 2020);
+
+console.log(ob1);
+
+let object2 = {
+  firstName: "Ryan",
+  lastName: "James",
+  age: 25,
+  experience: 4,
+};
+
+console.log(object2);
