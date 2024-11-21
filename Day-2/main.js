1442a0be3c81373564ad6f825a6814d5527ad82e
ryan-gregoire <ryangregoire286@gmail.com>
2024-11-21 12:47:23 -0500

    Added Part day-2

---
+++ Day-2/main.js
@@ -0,0 +1,11 @@
+let myValueisTrue = true;
+console.log(myValueisTrue);
+
+myValueisTrue = false;
+console.log(myValueisTrue);
+
+let notValueTrue = !true;
+console.log(notValueTrue);
+
+let notValueFalse = !false;
+console.log(notValueFalse);
