91cc18610b49224aa4bbf1cbcf29fd6858e9879e
ryan-gregoire <ryangregoire286@gmail.com>
2024-11-21 12:57:34 -0500

    Added Learned About Number Booleans and Null And Undefined

---
+++ Day-2/number.js
@@ -0,0 +1,14 @@
+let number1 = 1337;
+let number2 = 12;
+let number3 = 90;
+
+console.log(number1);
+console.log(number2);
+console.log(number3);
+
+console.log("MAX: " + Number.MAX_VALUE);
+console.log("MAX SAFE: " + Number.MAX_SAFE_INTEGER);
+console.log("MIN SAFE: " + Number.MIN_SAFE_INTEGER);
+console.log("MIN: " + Number.MIN_VALUE);
+
+let num1 = 1n;
